6982138bea09ab4454f79dbea81d9845c57e0d0f
Jung Yoonho <dbsgh756@naver.com>
2024-11-01 03:15:14 +0900

    [level 2] Title: 짝지어 제거하기, Time: 64.63 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,11 +1,11 @@
-function solution(s){
-    const stack = []
-    for (let char of s){
-        if(stack.length > 0 && stack[stack.length-1] === char){
-            stack.pop()
-        } else{
-        stack.push(char)            
+function solution(s) {
+    const stack = [];
+    for (let char of s) {
+        if (stack.length && stack[stack.length - 1] === char) {
+            stack.pop();
+        } else {
+            stack.push(char);
         }
     }
-    return stack.length === 0 ? 1 : 0
+    return stack.length === 0 ? 1 : 0;
 }
