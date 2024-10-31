d3fb45c7b30836c21e88fa3e581d9fb32a8c372a
Jung Yoonho <dbsgh756@naver.com>
2024-11-01 03:51:46 +0900

    [level 2] Title: 할인 행사, Time: 408.46 ms, Memory: 42.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,40 +1,34 @@
 function solution(want, number, discount) {
     var answer = 0;
     const dayCount = 10;
-    const groceryNeedList = new groceryNeedListGenerator(want, number);
-    const stack = [];
-    const endpointStack = [];
-    discount.forEach((item, index) => {
-        stack.push(item);
-        groceryNeedList[item] --;
+    
+    const groceryNeedList = want.reduce((acc, item, index) => {
+        acc[item] = number[index];
+        return acc;
+    }, {});
+    
+    const currentNeed = { ...groceryNeedList };
 
-        if(index >= 9){
-            if(index > 9) {
-                stack.shift();
-                const firstItemInStack = endpointStack[0];
-                groceryNeedList[firstItemInStack] ++;
-                endpointStack.pop();                
-            }
-            endpointStack.push(stack[0]);
-            isAllKeyZero(groceryNeedList) && answer ++;
-        }
-    });
+    for (let i = 0; i < dayCount; i++) {
+        currentNeed[discount[i]] = (currentNeed[discount[i]] || 0) - 1;
+    }
+    
+    if (isAllKeyZero(currentNeed)) answer++;
+
+    
+    for (let i = dayCount; i < discount.length; i++) {
+        const addItem = discount[i];
+        const removeItem = discount[i - dayCount];
+
+        currentNeed[addItem] = (currentNeed[addItem] || 0) - 1;
+        currentNeed[removeItem] = (currentNeed[removeItem] || 0) + 1;
+
+        if (isAllKeyZero(currentNeed)) answer++;
+    }
+
     return answer;
 }
 
-class groceryNeedListGenerator {
-    constructor(want, number){
-        want.forEach((item, index) => {
-            this[item] = number[index];
-        })
-    }
-}
-
-const isAllKeyZero = (obj) => {
-    for (let item in Object.keys(obj)){
-        if(obj[Object.keys(obj)[item]]){
-            return false;
-        }
-    }
-    return true;
+function isAllKeyZero(obj) {
+    return Object.values(obj).every(value => value === 0);
 }
