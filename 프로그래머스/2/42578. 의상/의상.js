b704981e8e957eaf4dcaaafa89aeec5616ada4ab
Jung Yoonho <dbsgh756@naver.com>
2024-11-30 11:29:30 +0900

    [level 2] Title: 의상, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -15,3 +15,13 @@ function solution(clothes) {
     answer --;
     return answer;
 }
+
+// [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+// headgear 2 eyewear 1
+// 안 입는 선택지 + 1
+// headgear 3 eyewear 2
+// 3 * 2 - 1 (모두 벗는 경우 제외)
+
+// [["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]	
+// face 3
+// 4 - 1
